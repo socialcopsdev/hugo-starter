27404bc5cb743bf5cc9172a8609d87c066fc7f3b
triestpa <patrick.triest@gmail.com>
2016-11-27 15:03:57 +0530

    Add static dir to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -9,88 +9,89 @@ const cleanCSS      = require('gulp-clean-css');
 const path =        require('path');
 const swPrecache =  require('sw-precache');
 
-const srcDir = 'src';
-const destDir = 'static';
-const publicDir = 'public';
+const srcDir = 'src/';
+const destDir = 'static/';
+const publicDir = 'public/';
+const swCachedFiletypesRegex = '**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'; 
 
 // SCSS pipeline with autoprefixer and cleancss
 gulp.task('scss', function() {
-    gulp.src(srcDir + '/scss/**/*.scss')
+    gulp.src(srcDir + 'scss/**/*.scss')
         .pipe(sass())
         .pipe(autoprefixer({
             browsers : ['last 20 versions']
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(concat('style.css'))
-        .pipe(gulp.dest(destDir + '/css'));
+        .pipe(gulp.dest(destDir + 'css'));
 });
 
 // CSS pipeline with autoprefixer and cleancss
 gulp.task('css', function() {
-    gulp.src(srcDir + '/css/**/*.css')
+    gulp.src(srcDir + 'css/**/*.css')
         .pipe(autoprefixer({
             browsers : ['last 20 versions']
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(concat('libs.css'))
-        .pipe(gulp.dest(destDir + '/css'));
+        .pipe(gulp.dest(destDir + 'css'));
 });
 
 // JS pipeline with babel and uglify
 // First build libraries, then custom files
 gulp.task('js', () => {
-    return gulp.src([srcDir + '/js/vendor/*.js' , srcDir + '/js/*.js'])
+    return gulp.src([srcDir + 'js/vendor/*.js' , srcDir + 'js/*.js'])
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(concat('scripts.js'))
         .pipe(uglify())
-        .pipe(gulp.dest(destDir + '/js'));
+        .pipe(gulp.dest(destDir + 'js'));
 });
 
 // Build js files for individual page behaviors
 gulp.task('jspages', () => {
-    return gulp.src([srcDir + '/js/pages/*.js'])
+    return gulp.src([srcDir + 'js/pages/*.js'])
         .pipe(babel({
             presets: ['es2015']
         }))
         .pipe(uglify())
-        .pipe(gulp.dest(destDir + '/js/pages'));
+        .pipe(gulp.dest(destDir + 'js/pages'));
 });
 
 // Optimize Images
 gulp.task('images', () =>
-    gulp.src(srcDir + '/images/**/*')
+    gulp.src(srcDir + 'images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest(destDir + '/images'))
+        .pipe(gulp.dest(destDir + 'images'))
 );
 
 // Generate Service Worker
 // The service worker will automatically cache all site resources in the browser for offline use.
 gulp.task('sw', function(callback) {
     swPrecache.write(path.join(destDir, 'service-worker.js'), {
-        staticFileGlobs: ['public' + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+        staticFileGlobs: [publicDir + swCachedFiletypesRegex],
         verbose: true,
-        stripPrefix: publicDir + '/',
+        stripPrefix: publicDir,
     }, callback);
 });
 
 // Copy the App Manifest to the static dir
 gulp.task('manifest', () =>
-    gulp.src(srcDir + '/manifest.json')
+    gulp.src(srcDir + 'manifest.json')
         .pipe(gulp.dest(destDir))
 );
 
 // Watch asset folder for changes
 gulp.task('watch', ['scss','css','images','js','jspages','manifest'], function () {
-    gulp.watch(srcDir + '/scss/**/*.scss', ['scss']);
-    gulp.watch(srcDir + '/images/**/*', ['images']);
-    gulp.watch(srcDir + '/js/**/*.js', ['js','jspages']);
-    gulp.watch(srcDir + '/css/**/*.css', ['css']);
-    gulp.watch(srcDir + '/manifest.json', ['manifest']);
+    gulp.watch(srcDir + 'scss/**/*.scss', ['scss']);
+    gulp.watch(srcDir + 'images/**/*', ['images']);
+    gulp.watch(srcDir + 'js/**/*.js', ['js','jspages']);
+    gulp.watch(srcDir + 'css/**/*.css', ['css']);
+    gulp.watch(srcDir + 'manifest.json', ['manifest']);
 });
 
 // Build assets
-gulp.task('build', ['scss','css','images','js', 'jspages','manifest']);
+gulp.task('build', ['scss','css','images','js','jspages','manifest']);
 
 gulp.task('default', ['build']);
